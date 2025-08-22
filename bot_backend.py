365c4adc22dbd7d32dd6bc2ed53db109cd6f87b6
Rustam <rustamvurdihanov@gmail.com>
2025-08-22 21:52:25 +0700

    Эхо-бот поддерживающий почти все виды апдейтов

--- bot_backend.py
+++ bot_backend.py
@@ -1,9 +1,8 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 
 from config import TOKEN
-
 
 
 bot = Bot(token=TOKEN)
@@ -11,18 +10,21 @@ dp = Dispatcher()
 
 
 @dp.message(Command(commands=['start']))
-async def process_start_command(message: Message):
+async def process_start_command(message: Message) -> None:
     await message.answer(text='Салам брат')
 
 
 @dp.message(Command(commands=['help']))
-async def process_help_command(message: Message):
+async def process_help_command(message: Message) -> None:
     await message.answer(text='Напиши мне что-нибудь и в ответ я пришлю тебе твое сообщение')
 
 
 @dp.message()
-async def send_echo(message: Message):
-    await message.reply(text=message.text)
+async def send_echo(message: Message) -> None:
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Данный тип апдейтов не поддерживается ')
 
 
 if __name__ == '__main__':
