45c09a005149c07772d41b74d51338ae696903e1
Rustam <rustamvurdihanov@gmail.com>
2025-08-23 02:33:27 +0700

    Игровой бот поддерживающий две игры: "Эхо-бот" и "Угадай число".

--- config.py
+++ config.py
@@ -4,3 +4,37 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('LZ_BOT_TOKEN')
+
+'''
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message, ContentType
+
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message) -> None:
+    await message.answer(text='Салам брат')
+
+
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message) -> None:
+    await message.answer(text='Напиши мне что-нибудь и в ответ я пришлю тебе твое сообщение')
+
+
+@dp.message()
+async def send_echo(message: Message) -> None:
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Данный тип апдейтов не поддерживается ')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
+'''
