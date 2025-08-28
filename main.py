c2577183aa0fc7461d49cd283087470dab35269b
Rustam <rustamvurdihanov@gmail.com>
2025-08-28 12:19:00 +0700

    Добавлена кнопка menu

--- main.py
+++ main.py
@@ -4,11 +4,24 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.types import BotCommand
 
 from config.config import Config, load_config
 from handlers import others, users
 
 logger = logging.getLogger(__name__)
+
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [
+        BotCommand(command='/start',
+                   description='Запуск бота'),
+        BotCommand(command='/help',
+                   description='Справка по работе бота'),
+        BotCommand(command='/stats',
+                   description='Статистика пользователя')
+    ]
+
+    await bot.set_my_commands(main_menu_commands)
 
 async def main():
     config: Config = load_config()
@@ -23,6 +36,7 @@ async def main():
     bot = Bot(token=config.bot.token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
     dp = Dispatcher()
+    dp.startup.register(set_main_menu)
 
     dp.include_router(users.router)
     dp.include_router(others.router)
